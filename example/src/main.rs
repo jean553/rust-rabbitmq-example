cb019155844d1c7c463cecac1bfe559df0ff5862
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-05-24 08:29:46 +0200

    create a session to the rabbitmq container when start the process

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,9 @@
+extern crate amqp;
+
+use amqp::Session;
+
 fn main() {
-    println!("Hello, world!");
+
+    const QUEUE_URL: &str = "amqp://rust_rabbitmq_example_queue//";
+    let mut session = Session::open_url(QUEUE_URL).unwrap();
 }
