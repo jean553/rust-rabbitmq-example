20ef8cbb4b4bfa5a6129ceb5276f1fa3339adfe4
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-05-24 20:42:44 +0200

    integrate the user actions

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,6 +3,9 @@ extern crate amqp;
 use amqp::Session;
 
 use std::thread::spawn;
+use std::io::stdin;
+
+const QUEUE_URL: &str = "amqp://rust_rabbitmq_example_queue//";
 
 /// Simulates a consumer (worker). Continuously checks for messages from the queue.
 fn get_queue_messages() {
@@ -10,15 +13,35 @@ fn get_queue_messages() {
 
 fn main() {
 
-    const QUEUE_URL: &str = "amqp://rust_rabbitmq_example_queue//";
+    /* initializes the producer */
+
     let mut session = Session::open_url(QUEUE_URL).unwrap();
-    let mut channel = session.open_channel(1).unwrap();
+    let mut _channel = session.open_channel(1).unwrap();
+
+    /* initializes the consumer */
 
     spawn(|| { get_queue_messages() });
 
+    /* user actions */
+
+    let mut input = String::new();
+
+    loop {
+        stdin().read_line(&mut input).expect("cannot get user input");
+
+        let input = input.trim();
+        if input == "exit" {
+            break;
+        }
+    }
+
+    /* send a successfull reply-code with a close-ok message
+       as we simply close the connection without any error
+       from the producer side */
+
     const CLOSE_REPLY_CODE: u16 = 200;
-    const CLOSE_REPLY_TEXT: &str = "closing";
-    channel.close(
+    const CLOSE_REPLY_TEXT: &str = "closing producer";
+    _channel.close(
         CLOSE_REPLY_CODE,
         CLOSE_REPLY_TEXT,
     ).unwrap();
